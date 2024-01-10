4cdf240bff64045deefb81fe7526602920346466
oxianglin <155654579+oxianglin@users.noreply.github.com>
2024-01-10 17:11:01 +0000

    Update main.py to support running main.py from a different directory

--- AMLS_23-24_SN20110935/main.py
+++ AMLS_23-24_SN20110935/main.py
@@ -2,10 +2,9 @@ import os
 import sys
 import argparse
 
-sys.path.insert(0, f'{os.getcwd()}/A')
-import pneumoniamnist as pnm
-
-sys.path.insert(0, f'{os.getcwd()}/B')
+sys.path.insert(0, os.path.join(os.path.dirname(__file__), 'A')) #To ensure that <the name of the directory that contains this file>/A is the first path in sys.path list for import to find module pneumoniamnist.py
+from pneumoniamnist import Pneumoniamnist
+sys.path.insert(0, os.path.join(os.path.dirname(__file__), 'B')) #To ensure that <the name of the directory that contains this file>/B is the first path in sys.path list for import to find module pathmnist.py
 from pathmnist import Pathmnist
 
 class Amls():
@@ -13,31 +12,31 @@ class Amls():
         super().__init__()
         self.mnist = args.mnist
         self.nettype = args.nettype
-        if (self.nettype > 1): #To change based on the number of nettype supported
+        if (self.nettype > 1): #To be changed based on the number of nettypes supported
             self.nettype = 1
-        self.lib = args.lib
         self.debug = args.debug
 
     @classmethod
-    def argparser(cls):
-        parser = argparse.ArgumentParser(description='Advnced machine learning system', formatter_class=argparse.ArgumentDefaultsHelpFormatter)
+    def argparser(cls): #argparser is my own AMLS member function/method 
+        #argparse is site package with class ArgumentParser
+        #ArgumentParser class has a member function called parse_args (returns value that we write into args)
+        parser = argparse.ArgumentParser(description='Applied Machine Learning Systems', formatter_class=argparse.ArgumentDefaultsHelpFormatter)
         parser.add_argument('-m', '--mnist', nargs='?', default='pathmnist', help='pathmnist or pneumoniamnist')
         parser.add_argument('-n', '--nettype', nargs='?', type=int, default=1, help='Self-defined neural network type')
-        parser.add_argument('-l', '--lib', action='store_true', help='Use medmnist library')
         parser.add_argument('-dbg', '--debug', action='store_true', help='Print debug messages')
         return parser
     
     def run_pathmnist(self):
-        mnist = Pathmnist(self.debug)
-        mnist.data_loading(self.lib)
+        mnist = Pathmnist(self.debug) #Instantiate an object of class Pathmnist defined in B/pathmnist.py.
         mnist.define_model(self.nettype, mnist.n_channels, mnist.n_classes)
         mnist.train_model(self.nettype)
-        mnist.test_model(self.nettype,'test')
+        mnist.test_model(self.nettype, 'test')
 
     def run_pneumoniamnist(self):
-        mnist = pnm.Pneumoniamnist()
-        net = pnm.Net(in_channels=mnist.n_channels, num_classes=mnist.n_classes, type=self.nettype)
-        net.hello()
+        mnist = Pneumoniamnist(self.debug) #Instantiate an object of class Pneumoniamnist defined in A/pneumoniamnist.py.
+        mnist.define_model(self.nettype, mnist.n_channels, mnist.n_classes)
+        mnist.train_model(self.nettype)
+        mnist.test_model(self.nettype, 'test')
 
 def main():
     amls = Amls(Amls.argparser().parse_args())
