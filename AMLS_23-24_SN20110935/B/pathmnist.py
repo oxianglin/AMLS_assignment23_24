4ca0ccdee2908ad6baa80cf32b659adb27ec63d8
oxianglin <155654579+oxianglin@users.noreply.github.com>
2024-01-10 17:14:10 +0000

    Update pathmnist.py so that data is processed using my own code adopted from medmnist library
    Data processing using my own code adopted from medmnist library, omitted the option of using code directly from medmnist library

--- AMLS_23-24_SN20110935/B/pathmnist.py
+++ AMLS_23-24_SN20110935/B/pathmnist.py
@@ -8,8 +8,8 @@ import torchvision.transforms as transforms
 import os
 import numpy as np
 import medmnist
+from medmnist import INFO
 from sklearn.metrics import accuracy_score
-from medmnist import INFO, Evaluator
 
 DEBUG = False
 
@@ -77,7 +77,7 @@ class Net(nn.Module):
 
 class data_set(data.Dataset):
     data_file = ''
-    def __init__(self, split, transform=None, target_transform=None, as_rgb=False):
+    def __init__(self, split, transform=None, target_transform=None, as_rgb=False): #Initialise dataset.
         if (self.data_file == ''):
             self.search_data()
         npz_file = np.load(self.data_file)
@@ -120,12 +120,12 @@ class data_set(data.Dataset):
         else:
             raise ValueError
   
-    def __len__(self):
+    def __len__(self): #For DataLoader class to get the number of items in the dataset.
         return self.imgs.shape[0]
   
-    def __getitem__(self, index):
+    def __getitem__(self, index): #For DataLoader class to get the items.
         img, target = self.imgs[index], self.labels[index].astype(int)
-        img = Image.fromarray(img)
+        img = Image.fromarray(img) #Create image object from numpy array.
         if self.as_rgb:
             img = img.convert('RGB')
         if self.transform is not None:
@@ -135,23 +135,11 @@ class data_set(data.Dataset):
         return img, target
 
     def search_data(self):
-        exist = 0
-        if not os.path.exists(f'{os.getcwd()}{os.sep}Datasets{os.sep}PathMNIST{os.sep}pathmnist.npz'):
-            print(f'{os.getcwd()}{os.sep}Datasets{os.sep}PathMNIST{os.sep}pathmnist.npz not found, searching in other directories')
-            for (root,dirs,files) in os.walk(os.sep):
-                rt = root.split(os.sep)
-                for dir in dirs:
-                    if (rt[len(rt)-1] == 'Datasets' and dir == 'PathMNIST' and os.path.exists(f'{os.path.join(root, dir)}{os.sep}pathmnist.npz')):
-                        exist = 1
-                        print(f'{os.path.join(root, dir)}{os.sep}pathmnist.npz found')
-                        data_set.data_file = f'{os.path.join(root, dir)}{os.sep}pathmnist.npz'
-                        break
-                if exist:
-                    break
-            if not exist:
-                raise RuntimeError('pathmnist.npz not found')
-        else:
-            data_set.data_file = f'{os.getcwd()}{os.sep}Datasets{os.sep}PathMNIST{os.sep}pathmnist.npz'
+        dir = os.path.dirname(__file__) #<The directory of this file> which is <upper directories/amls/B>.
+        ds = dir.split(os.sep)
+        ds.pop(len(ds)-1) #Move one level up in directory.
+        dir = os.sep.join(ds)
+        data_set.data_file = f'{dir}{os.sep}Datasets{os.sep}PathMNIST{os.sep}pathmnist.npz'
 
 class Pathmnist():
     def __init__(self, debug):
@@ -162,53 +150,44 @@ class Pathmnist():
         self.n_channels = info['n_channels']
         self.n_classes = len(info['label'])
         self.data_transform = transforms.Compose([transforms.ToTensor(),transforms.Normalize(mean=[.5], std=[.5])])
+        print('Use code extracted minimally from medmnist library for datatset processing')
+        print('Dataset objects to be initiated from data_set class contain pathmnist data')
+        self.train_dataset = data_set(split='train', transform=self.data_transform)
+        self.val_dataset = data_set(split='val', transform=self.data_transform)
+        self.test_dataset = data_set(split='test', transform=self.data_transform)
 
-    def data_loading(self, lib, batch_size=128):
-        if (lib):
-            print('Use medmnist library for dataset processing')
-            DataClass = getattr(medmnist, info['python_class'])
-            print('Dataset objects to be initiated from DataClass contain pathmnist data')
-            self.train_dataset = DataClass(split='train', transform=self.data_transform, download=True)
-            self.val_dataset = DataClass(split='val', transform=self.data_transform, download=True)
-            self.test_dataset = DataClass(split='test', transform=self.data_transform, download=True)
-            print('The data to be encapsulated into DataLoader form')
-            self.train_loader = data.DataLoader(dataset=self.train_dataset, batch_size=batch_size, shuffle=True)
-            self.train_loader_at_eval = data.DataLoader(dataset=self.train_dataset, batch_size=2*batch_size, shuffle=False)
-            self.val_loader = data.DataLoader(dataset=self.val_dataset, batch_size=batch_size, shuffle=False)
-            self.test_loader = data.DataLoader(dataset=self.test_dataset, batch_size=2*batch_size, shuffle=False)
-        else:
-            print('Use code extracted minimally from medmnist library for datatset processing')
-            print('Dataset objects to be initiated from data_set class contain pathmnist data')
-            self.train_dataset = data_set(split='train', transform=self.data_transform)
-            self.val_dataset = data_set(split='val', transform=self.data_transform)
-            self.test_dataset = data_set(split='test', transform=self.data_transform)
-            print('The data to be encapsulated into DataLoader form')
-            self.train_loader = data.DataLoader(dataset=self.train_dataset, batch_size=batch_size, shuffle=True)
-            self.train_loader_at_eval = data.DataLoader(dataset=self.train_dataset, batch_size=2*batch_size, shuffle=False)
-            self.val_loader = data.DataLoader(dataset=self.val_dataset, batch_size=batch_size, shuffle=False)
-            self.test_loader = data.DataLoader(dataset=self.test_dataset, batch_size=2*batch_size, shuffle=False)
+    def data_loading(self, batch_size=128):
+        print('The data to be encapsulated into DataLoader form')
+        self.train_loader = data.DataLoader(dataset=self.train_dataset, batch_size=batch_size, shuffle=True)
+        self.val_loader = data.DataLoader(dataset=self.val_dataset, batch_size=batch_size, shuffle=False)
+        self.test_loader = data.DataLoader(dataset=self.test_dataset, batch_size=2*batch_size, shuffle=False)
     
     def define_model(self, nettype, n_channels, n_classes, learningrate=0.001):
         self.model = Net(in_channels=n_channels, num_classes=n_classes, nettype=nettype)   
-        print('Define loss function and optimizer')
-        #multi-class
-        self.criterion = nn.CrossEntropyLoss()
+        print('Define loss function and optimiser')
+        if self.task == "multi-label, binary-class":
+            self.criterion = nn.BCEWithLogitsLoss()
+        else:
+            self.criterion = nn.CrossEntropyLoss()
         self.optimizer = optim.SGD(self.model.parameters(), lr=learningrate, momentum=0.9)
 
     def train_model(self, nettype, epoch=3):
         self.val_acc = []
         self.saved_model = []
+        self.data_loading()
         for epoch in range(epoch):
             print(f'Train the model with train set at epoch {epoch}')
             self.model.train()
             for inputs, targets in tqdm(self.train_loader):
-                # forward + backward + optimize
+                # forward + backward + optimise
                 self.optimizer.zero_grad()
                 outputs = self.model(inputs, nettype)                
-                #multi-class
-                targets = targets.squeeze().long()
-                loss = self.criterion(outputs, targets)
-
+                if self.task == 'multi-label, binary-class':
+                    targets = targets.to(torch.float32)
+                    loss = self.criterion(outputs, targets)
+                else:
+                    targets = targets.squeeze().long() #Remove axes of length one then convert to int64.
+                    loss = self.criterion(outputs, targets)
                 loss.backward()
                 self.optimizer.step()
             self.val_acc.append(self.test_model(nettype, 'val'))
@@ -231,14 +210,31 @@ class Pathmnist():
         with torch.no_grad():
             for inputs, targets in tqdm(data_loader):
                 outputs = self.model(inputs, nettype)
-                targets = targets.squeeze().long()
-                outputs = outputs.softmax(dim=-1)
-                targets = targets.float().resize_(len(targets), 1)
+                if self.task == 'multi-label, binary-class':
+                    targets = targets.to(torch.float32)
+                    outputs = outputs.softmax(dim=-1)
+                else:
+                    targets = targets.squeeze().long()
+                    outputs = outputs.softmax(dim=-1)
+                    targets = targets.float().resize_(len(targets), 1)
                 y_true = torch.cat((y_true, targets), 0)
                 y_score = torch.cat((y_score, outputs), 0)
-            y_true = y_true.numpy().squeeze()
-            y_score = y_score.detach().numpy().squeeze()
-            #multi-class
-            accuracy = accuracy_score(y_true, np.argmax(y_score, axis=-1))
+            y_true = y_true.numpy().squeeze() #Convert tensor to numpy then remove axes of length one.
+            y_score = y_score.numpy().squeeze()
+            if self.task == 'multi-label, binary-class':
+                y_pre = y_score > 0.5
+                acc = 0
+                for label in range(y_true.shape[1]):
+                    label_acc = accuracy_score(y_true[:, label], y_pre[:, label])
+                    acc += label_acc
+                accuracy = acc / y_true.shape[1]
+            elif self.task == 'binary-class':
+                if y_score.ndim == 2:
+                    y_score = y_score[:, -1]
+                else:
+                    assert y_score.ndim == 1
+                accuracy = accuracy_score(y_true, y_score > 0.5)
+            else:
+                accuracy = accuracy_score(y_true, np.argmax(y_score, axis=-1)) #np.argmax returns the indices of the maximum values along the last axis.
             print('%s acc:%.3f' % (split, accuracy))
             return accuracy
